55164bd1810d65ab4f57ec92a8a4fa1417643147
Stanisław Nieradko <stanislaw@nieradko.com>
2025-10-19 15:25:10 +0200

    feat: initialize repository and Lab01

---
+++ Lab01/2-UTNodeInBrowser/src/worker.js
@@ -0,0 +1,5 @@
+function Worker(name) {
+	this.Introduce = function() { return "Hello I'm " + name; }
+		this.DoJob = function(task) { return "Worker " + name + " does " + task; }
+} 
+
---
+++ Lab01/3-Project/test/SimpleTest.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('canary-tests', function () {
+    it('should always pass the canary test', function () {
+        expect(true).to.eql(true);
+    });
+});
